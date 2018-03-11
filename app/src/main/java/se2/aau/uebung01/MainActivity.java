0f80acd3b163074284d7a50121f4f7a6ffedec8f
Andreas Mairitsch <anmairitsch@edu.aau.at>
2018-03-11 15:02:12 +0100

    final commit

--- app/src/main/java/se2/aau/uebung01/MainActivity.java
+++ app/src/main/java/se2/aau/uebung01/MainActivity.java
@@ -1,7 +1,10 @@
-xxpackage se2.aau.uebung01;
+package se2.aau.uebung01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void palindrome(View view){
+        EditText text = findViewById(R.id.editText);
+        TextView outputView = findViewById(R.id.textView3);
+        String input=text.getText().toString();
+
+        if(text.length()>=5){
+            boolean isPalindrom = isPalindrome(input);
+
+            if(isPalindrom){
+                outputView.setText("ist ein Palindrom");
+            }
+            else{
+                outputView.setText("Ist kein Palindrom");
+            }
+        }
+        else{
+            if(input.length()==0){
+               outputView.setText("Eingabetext ist leer");
+            }else {
+               outputView.setText("Eingabetext ist zu kurz");
+            }
+        }
+
+    }
+    public static boolean isPalindrome(String str){
+        return str.equals(new StringBuilder(str).reverse().toString());
     }
 }
